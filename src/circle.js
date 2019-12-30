ce27f6d642238c90bdcd1167a68414b415fb11ef
kvoncode <kvoncode@gmail.com>
2019-12-30 16:25:36 +0900

    prettify code

--- src/circle.js
+++ src/circle.js
@@ -1,7 +1,6 @@
 const { PI } = Math;
 
 exports.area = r => {
-  console.log("circle.js is called as main script? :",require.main === module);
+  console.log("circle.js is called as main script? :", require.main === module);
   return PI * r ** 2;
 };
-
--- src/foo.js
+++ src/foo.js
@@ -1,9 +1,11 @@
 const circle = require("./circle");
 
-console.log(circle.area(1))
+const radius = 1;
 
-// // console.log(module.exports === exports)
-// console.log(require.main === module)
-// console.log(require.main.filename)
-// console.log(__filename)
-console.log(require.resolve)
+console.log(`area of the circle with radius ${radius} :`, circle.area(radius));
+
+console.log("module.exports === exports ?", module.exports === exports);
+console.log("foo.js is called as main script? :", require.main === module);
+console.log("require.main.filename :", require.main.filename);
+console.log("__filename :", __filename);
+console.log("require.resolve :", require.resolve);
